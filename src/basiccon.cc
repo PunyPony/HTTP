e56f545fc4ba2e6948c2b35b54dd395e21fa711d
Nicolas Balagny-Carbuccia <nicolas.balagny-carbuccia@epita.fr>
2018-02-08 05:50:54 +0100

    Add single user connection (msg formats ignored)

---
+++ src/basiccon.cc
@@ -0,0 +1,195 @@
+#include <iostream>
+#include <arpa/inet.h>
+#include <unistd.h>
+#include <fstream>
+//#include <string>
+//#define _DEFAULT_SOURCE
+//#define _GNU_SOURCE
+
+enum error_type
+{
+    ACCESS_DENIED = 401,
+    FILE_NOT_FOUND = 404,
+    INTERNAL_ERROR = 500,
+    NIQUE_TA_MERE = 6969
+};
+
+enum request_type
+{
+    RQFILE
+};
+
+struct fileparams
+{
+    std::string path;
+};
+
+int init_connection();
+int wait_for_client(int sock);
+std::string get_request(int client_sock);
+void dispatch_request(int client_sock, std::string request);
+request_type get_request_type(std::string request, void*& params);
+std::string forge_error_response(error_type err);
+std::string forge_response(request_type, void* params);
+void send_reponse(int sock, std::string response);
+
+int listen_port = 6666;
+
+int main(int argc, char** argv)
+{
+    if(argc > 1)
+	listen_port = atoi(argv[1]);
+    int sock = init_connection();
+    while(1)
+    {
+	int client_sock = wait_for_client(sock);
+    
+	std::string request = get_request(client_sock);
+	if(request == "TA GUEULE\r\n")
+	    break;
+	std::cout << "New request from client: " << std::endl;
+	std::cout << request << "End of request" << std::endl;
+
+	dispatch_request(client_sock, request);
+    }
+        
+    close(sock);
+}
+
+//fixme replace error fct call with error message where needed (internal error: 5**)
+void error(std::string msg, int code)
+{
+    std::cerr << msg <<std::endl;
+    exit(code);
+}
+
+int init_connection()
+{
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if(sock == -1)
+	error("sock", 1);
+    struct sockaddr_in addrin
+    {
+	.sin_family = AF_INET,
+	.sin_port = htons(listen_port),
+	    .sin_addr = {}
+    };
+    if(!inet_aton("0.0.0.0", &addrin.sin_addr))
+	error("inet_aton", 1);
+    if(bind(sock, (sockaddr*)&addrin, sizeof(addrin)) == -1)
+	error("bind", 1);
+    if(listen(sock, 1) == -1) //limited to 1 connection simultaazef,szoldgkg
+	error("listen", 1);
+    return sock;
+}
+
+int wait_for_client(int sock)
+{
+    struct sockaddr_in sockin{};
+    socklen_t socklen = sizeof(struct sockaddr_in);
+    int connection_sock = accept4(sock,  (sockaddr*)&sockin, &socklen, 0);//, SOCK_NONBLOCK);
+    if(connection_sock == -1)
+	error("accept4", 1);
+    std::cout << "salut" << std::endl;
+    return connection_sock;
+}
+
+bool is_request_full(std::string request)
+{
+    //fixme
+    return true;
+}
+
+//fixme: possible? message splitted in multiple packets -> isolate users -> keep list of incomplete requests -> add timeout to prevent dos
+//fixme: check if end of request (warning: malformed ones)
+std::string get_request(int client_sock)
+{
+    size_t blocksize = 500;
+    size_t totalsize = 0;
+    std::string request;
+    int res = 0;
+    while(res != -1)
+    {
+	request.reserve(totalsize + blocksize);
+	request.resize(totalsize + blocksize);
+	res = read(client_sock, &request[totalsize], blocksize);
+	totalsize += res;
+	if(is_request_full(request)) //warning: request not truncated to real size at this point (->pass size, resize may cost much)
+	{
+	    totalsize -= 1;
+	    break;
+	}
+    }
+    request.resize(totalsize + 1);
+    return request;
+}
+
+void dispatch_request(int client_sock, std::string request)
+{
+    void* params = NULL;
+    request_type rqtype = get_request_type(request, params);
+    std::string response = forge_response(rqtype, params);
+    
+    std::cout << "response is" << std::endl << response << "end of response" << std::endl;
+    std::cout << "sending response:..." << std::endl;
+    
+    send_reponse(client_sock, response);
+    
+    std::cout << "done" << std::endl;
+    close(client_sock); //fixme: should we close this
+}
+
+request_type get_request_type(std::string request, void*& params)
+{
+    //fixme: mutiple request types possible?
+    params = new struct fileparams;
+    ((struct fileparams*)params)->path = "testfile";
+    return RQFILE;
+}
+
+std::string forge_error_response(error_type err)
+{
+    return "ERROR " + std::to_string(err) + ": ça marche pas lol \n";
+}
+
+//assuming the only request is a file retrieval
+std::string forge_response(request_type rqtype, void* params)
+{
+    std::string result;
+    switch(rqtype)
+    {
+    case RQFILE:
+	struct fileparams* definedparams = (struct fileparams*)params;
+	std::string path = definedparams->path;
+	//fixme: check path bounds
+	std::ifstream file(path);
+	if (!file.good())
+	    result = forge_error_response(FILE_NOT_FOUND); //fixme: check error type(not found, cannot open...)
+	else
+	{
+	    std::string content;
+	    file >> content;
+    
+	    file.seekg(0, std::ios::end);   
+	    content.reserve(file.tellg());
+	    file.seekg(0, std::ios::beg);
+    
+	    content.assign((std::istreambuf_iterator<char>(file)),
+			   std::istreambuf_iterator<char>());
+	    result = content;
+	}
+	delete definedparams;
+	return result;
+    }
+    return forge_error_response(NIQUE_TA_MERE); //or INTERNAL_ERROR for less fun
+}
+
+void send_reponse(int client_sock, std::string response)
+{
+    int res = write(client_sock, &response[0], response.size()); //should we write <end of file>?
+    if(res == -1)
+	error("write", 2);
+    else
+	if(res != response.size())
+	    error("write: wrong size written", 2);
+}
