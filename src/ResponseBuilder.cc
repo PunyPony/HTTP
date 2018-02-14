09134c1f4185a5ac3036b8e77652000c35271a61
Nicolas Roger <nicolas.roger@epita.fr>
2018-02-14 18:58:52 +0100

    Build response parsing

--- src/ResponseBuilder.cc
+++ src/ResponseBuilder.cc
@@ -1,69 +1,105 @@
 #include <ResponseBuilder.hh>
 #include <iostream>
 #include <string>
+#include <sstream>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <string.h>
+//#include <cstdlib>
+//#include <cstring>
+#include <unistd.h>
 
 ResponseBuilder::ResponseBuilder()
 {}
 
-/*
-std::string get_request(int client_sock)
+std::string get_request(int socketFileDescriptor)
 {
-    size_t blocksize = 500;
-    size_t totalsize = 0;
-    std::string request;
-    int res = 0;
-    while(res != -1)
+    char *buffer;
+    int BUFFER_SIZE = 4096;
+    bzero(buffer, BUFFER_SIZE);
+    std::stringstream readStream;
+    int readResult = 0;
+    std::cout << "Receiving chunk... ";
+    while ((readResult = read(socketFileDescriptor, buffer, BUFFER_SIZE)) > 0)
+      readStream << buffer;
+
+    if (readResult == 0)
     {
-	request.reserve(totalsize + blocksize);
-	request.resize(totalsize + blocksize);
-	res = read(client_sock, &request[totalsize], blocksize);
-	totalsize += res;
-	if(is_request_full(request)) //warning: request not truncated to real size at this point (->pass size, resize may cost much)
-	{
-	    totalsize -= 1;
-	    break;
-	}
+      // Timeout - handle that. You could try waiting again, close the socket...
     }
-    request.resize(totalsize + 1);
-    return request;
+    if (readResult == -1)
+    {
+      // Handle the error
+    }
+
+    std::string readData = readStream.str();
+    std::cout << readData << std::endl;
+    return readData;
 }
 
-request_type get_request_type(std::string request, void*& params)
+int parse_request_line(std::string request)
 {
-    request = request;
-    //fixme: mutiple request types possible?
-    params = new struct fileparams;
-    ((struct fileparams*)params)->path = "testfile";
-    return RQFILE;
+  return 0;
 }
 
-std::string forge_response(request_type rqtype, void* params)
+int parse_general_header(std::string request)
 {
-    std::string result;
-    switch(rqtype)
-    {
-    case RQFILE:
-	struct fileparams* definedparams = (struct fileparams*)params;
-	std::string path = definedparams->path;
-	//fixme: check path bounds
-	std::ifstream file(path);
-	if (!file.good())
-	    result = forge_error_response(FILE_NOT_FOUND); //fixme: check error type(not found, cannot open...)
-	else
-	{
-	    std::string content;
-	    file >> content;
-    
-	    file.seekg(0, std::ios::end);   
-	    content.reserve(file.tellg());
-	    file.seekg(0, std::ios::beg);
-    
-	    content.assign((std::istreambuf_iterator<char>(file)),
-			   std::istreambuf_iterator<char>());
-	    result = content;
-	}
-	delete definedparams;
-	return result;
-    }
-    return forge_error_response(NIQUE_TA_MERE); //or INTERNAL_ERROR for less fun
-}*/
+  return 0;
+}
+
+int parse_request_header(std::string request)
+{
+  return 0;
+}
+
+int parse_entity_header(std::string request)
+{
+  return 0;
+}
+
+int parse_body(std::string request)
+{
+  return 0;
+}
+
+std::string get_token(const std::string& request, const std::string& delimiter)
+{
+  return request.substr(0, request.find(delimiter));
+}
+
+std::string get_request_rest(std::string& request, const std::string& delimiter)
+{
+  return request.erase(0, request.find(delimiter) + delimiter.length());
+}
+
+int parse_request(std::string request)
+{
+  /*
+  Request = Request-Line
+  * (( general-header | request-header | entity-header ) CRLF )
+  CRLF
+  [ message-body ]
+  */
+  std::string delimiter = "\r\n";
+
+  if (parse_request_line(get_token(request, delimiter)));
+    return -1;
+
+  std::string rest = get_token(get_request_rest(request, delimiter), delimiter);
+  while (!rest.empty())
+  {
+    if (parse_general_header(rest) == -1
+        && parse_request_header(rest) == -1
+        && parse_entity_header(rest) == -1)
+      return -1;
+    std::string rest = get_token(get_request_rest(request, delimiter), delimiter);
+  }
+  if (parse_body(get_token(get_request_rest(request, delimiter), delimiter)))
+    std::cout << "Body found" << std::endl;
+
+  return 0;
+}
+
