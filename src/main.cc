184861e3043348b83d563e2811ba3855186828b3
Vladimir Kopecky <vladimir.kopecky@r07p04.midlab.sm.cri.epita.net>
2018-02-16 15:41:12 +0100

    [HTTPD] Modified error codes in main

--- src/main.cc
+++ src/main.cc
@@ -23,31 +23,39 @@ int main(int argc, char* argv[])
   const char* arg = argv[1];
   std::ifstream file(arg);
 
+  /*
   if (not file.good())
     return 1;
-
-  //Parse the file
-  toml::Data data = toml::parse(file);
-  /*
-  if (data == None)
-    return 2; il faut trouver la valeur de data quand ça foire*/
-
-  //FIXME CHECK VALUES - IF NONE THEN RETURN 2
-
-  //Get 2 first elements: log_file and dry_run
-//  std::string log_file = toml::get<toml::String>(data.at("log_file"));
-  bool dry_run = toml::get<bool>(data.at("dry_run"));
+  */
   
+  bool dry_run = false;
+  std::string log_file;
+  int nbserv = 0;
+
+  try{
+
+    //Parse the file
+    toml::Data data = toml::parse(file);
+
+    //Get 2 first elements: log_file and dry_run
+
+    try{
+       dry_run = toml::get<bool>(data.at("dry_run"));
+    }catch(...){
+    }
+
+    try{
+      log_file = toml::get<toml::String>(data.at("log_file"));
+    }catch(...){
+    }
   
     //Create array of server to delimit each server config
     std::vector<toml::Table> server = toml::get<toml::Array<toml::Table> >(data.at("server"));
 
-    int nbserv = 0;
     for (auto it = server.cbegin(); it != server.cend(); ++it)
       nbserv++;
 
-   // ServerConfig *server_array[nbserv];
-   
+    // ServerConfig *server_array[nbserv]; 
     std::vector<ServerConfig*> server_array(nbserv);
 
     for (int i = 0; i < nbserv; i++)
@@ -58,7 +66,7 @@ int main(int argc, char* argv[])
       if (server.at(i).count("server_name"))
         server_array[i]->set_server_name(toml::get<toml::String>(
               server.at(i).at("server_name")));
-
+    
       if (server.at(i).count("port"))
         server_array[i]->set_port(toml::get<toml::String>(
             server.at(i).at("port")));
@@ -104,59 +112,45 @@ int main(int argc, char* argv[])
     for (int i = 0; i < nbserv; i++)
     {
       if (!server_array[i]->get_server_name().get())
+      {
+        std::cerr << "Missing server_name." << std::endl;
         return 2;
+      }
       if (!server_array[i]->get_port().get())
+      {
+        std::cerr << "Missing port." << std::endl;
         return 2;
+      }
       if (!server_array[i]->get_ip().get())
+      {
+        std::cerr << "Missing ip." << std::endl;
         return 2;
+      }
       if (!server_array[i]->get_root_dir().get())
+      {
+        std::cerr << "Missing root_dir." << std::endl;
         return 2;
+      }
     }
-  
-  
-/* 
-  for (int i = 0; i < nbserv; i++)
-  {
-    std::cout << server_array[i]->get_port().getparam() << std::endl;
-    std::cout << server_array[i]->get_ip().getparam() << std::endl;
-    if (server_array[i]->get_cgi_ext().get())
-      std::cout << "Param: true" << std::endl;
-    else
-      std::cout << "Param: false" << std::endl;
-  }
-*/
+    
+    std::vector<HTTPServerOptions> servers_options;
+    for (int i = 0; i < nbserv; i++)
+    {
+      HTTPServerOptions options(atoi(server_array[i]->get_port().getparam().c_str()), 8, server_array[i]->get_ip().getparam());
+      servers_options.push_back(options);
+    }
 
     //if dry_run set to true, just check and return. Launch server otherwise.
     if (!dry_run)
     {
-      std::vector<HTTPServerOptions> servers_options;
-      for (int i = 0; i < nbserv; i++)
-      {
-        HTTPServerOptions options(atoi(server_array[i]->get_port().getparam().c_str()), 8, server_array[i]->get_ip().getparam());
-        servers_options.push_back(options);
-      }
-
-      //Check log_file
-      std::string log_file;
-          try {
-               log_file = toml::get<toml::String>(data.at("log_file"));
-               std::cout << "Log file activated" << std::endl;
-              }
-          catch (...){
-              std::cout << "No log file required." <<std::endl;
-          }
-
-          try {
-            if (data.count("log_file"))
-              std::cout << "Launch server with log_file" << std::endl;
-          }
-          catch(...){
-            std::cout << "Launch server without log_file" << std::endl;
-          }
-
       ServersHandler servers_handler(servers_options);
       return 0;
     }
     else
       return 0;
+
+  }catch(...){
+    std::cerr << "Parsing error." <<std::endl;
+    return 2;
+  }
 }
